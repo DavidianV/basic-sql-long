2069a73dc5c8ee83339a15c4be0282e753c8eea6
root <root@WOPR>
2023-08-23 19:39:18 -0400

    good-coffee-worker

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,1 +1,18 @@
 -- Your code here
+
+drop table if exists customers;
+create table customers (
+    id integer primary key autoincrement,
+    name varchar(40) not null,
+    phone decimal(10,0),
+    email varchar(255) unique,
+    points integer not null default 5,
+    created_at timestamp default CURRENT_TIMESTAMP not null
+);
+
+drop table if exists coffee_orders;
+create table coffee_orders (
+    id integer primary key autoincrement,
+    is_redeemed boolean default 0,
+    ordered_at timestamp default CURRENT_TIMESTAMP NOT null
+);
--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,1 +1,41 @@
 -- Your code here
+insert into customers (name, phone)
+values ("Rachel", 1111111111);
+
+insert into coffee_orders (is_redeemed)
+values (0);
+
+insert into customer (name, email, phone) values
+("Monica", "monica@friends.show", 2222222222),
+("Pheobe", "phoebe@friends.show", 3333333333);
+
+insert into coffee_orders (is_redeemed) values
+(0),
+(0),
+(0);
+
+update customers set points=8 where name="Phoebe";
+
+insert into coffee_orders (is_redeemed) values
+(0),
+(0),
+(0),
+(0);
+
+insert into coffee_orders (is_redeemed) values
+(0),
+(0),
+(0),
+(0);
+
+select points from customers where name="Rachel";
+select points from customers where name="Monica";
+update customers set points=10 where name="Rachel";
+update customers set points=9 where name="Monica";
+
+select points from customers where name="Monica";
+
+select points from customers where name="Rachel";
+
+insert into coffee_orders (is_redeemed)
+values (1);
